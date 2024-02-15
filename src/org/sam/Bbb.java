54b8be6addb8e76870e9cb9b26a5f5581c9602ca
jeevithamangai <159793967+jeevithamangai@users.noreply.github.com>
2024-02-15 23:31:20 +0530

    Update Bbb.java

--- src/org/sam/Bbb.java
+++ src/org/sam/Bbb.java
@@ -4,7 +4,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Bbb {
+	public void t1
+	{
+		system.out.println("test1");
+	}
 	public static void main(String[] args) {
+		
 		System.out.println("browser launching");
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
